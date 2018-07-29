1c48ddfdbc01f9b01a50946beaf5addc32bc1b7e
KengoWada <louiskengo3@gmail.com>
2018-07-29 10:57:55 +0300

    Corrected indentation.

--- pig_latin.py
+++ pig_latin.py
@@ -1,38 +1,37 @@
 
-# This is written in python3 
 
 win = input('Enter a word:') # Asks user for input
 
-def pig_latin(kigambo): # Define  function that takes one paramater
+def pig_latin(kigambo): # Define  function that takes one parameter
 
-		if kigambo != str(kigambo):
-			raise ValueError('Only words allowed.')
+	if kigambo != str(kigambo):
+		raise ValueError('Only words allowed.')
+		
+	kigambo = kigambo.lower() # This changes the string input to lowercase
+	kigambo = list(kigambo) # This makes the input string a list
+	i = 0
 
-		kigambo = kigambo.lower() # This changes the string input to lowercase
-		kigambo = list(kigambo) # This makes the input string a list
-		i = 0
-	
-		for letter in kigambo: 
-			nyukuta = kigambo[0]
-	
-			if i == 0:
-				if (nyukuta == 'a' or nyukuta == 'e' or nyukuta == 'i' or nyukuta == 'o' or nyukuta == 'u'):
-					kigambo.append('way')
-					word = ''.join(kigambo)
-					break
-				else:
-					kigambo.append(kigambo.pop(kigambo.index(letter)))
+	for letter in kigambo: 
+		nyukuta = kigambo[0]
+
+		if i == 0:
+			if (nyukuta == 'a' or nyukuta == 'e' or nyukuta == 'i' or nyukuta == 'o' or nyukuta == 'u'):
+				kigambo.append('way')
+				word = ''.join(kigambo)
+				break
 			else:
-				if (nyukuta == 'a' or nyukuta == 'e' or nyukuta == 'i' or nyukuta == 'o' or nyukuta == 'u'):
-					kigambo.append('ay')
-					word = ''.join(kigambo)
-					break
-				else:
-					kigambo.append(kigambo.pop(kigambo.index(letter)))
-	
-			i += 1
-	
-		return word
+				kigambo.append(kigambo.pop(kigambo.index(letter)))
+		else:
+			if (nyukuta == 'a' or nyukuta == 'e' or nyukuta == 'i' or nyukuta == 'o' or nyukuta == 'u'):
+				kigambo.append('ay')
+				word = ''.join(kigambo)
+				break
+			else:
+				kigambo.append(kigambo.pop(kigambo.index(letter)))
+
+		i += 1
+
+	return word
 
 print(pig_latin(win))
 
